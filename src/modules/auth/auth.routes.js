0280d7ada30dc4a85576fb0efede14596299511f
ikartikeyo <kartikeypandey0011@gmail.com>
2026-05-19 12:12:41 +0530

    casul commit

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -3,7 +3,7 @@ const controller = require('./auth.controller')
 
 router.post('/signup', (req, res) => controller.signup(req, res))
 router.post('/login', (req, res) => controller.login(req, res))
-
+ 
 // NEW (does not break existing)
 router.post('/refresh', (req, res) => controller.refresh(req, res))
 router.post('/logout', (req, res) => controller.logout(req, res))
